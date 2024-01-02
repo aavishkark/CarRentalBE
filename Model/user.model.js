085e422b0ca30fc82d3ad5aba9880d69e32109b3
aviatalma <aavishkark98@gmail.com>
2024-01-02 11:37:47 +0530

    Second commit

---
+++ Model/user.model.js
@@ -0,0 +1,17 @@
+const mongoose=require("mongoose")
+
+const userSchema=mongoose.Schema({
+    email: String,
+    password: String,
+    username: String,
+    pastRides: Array,
+    city: String,
+    activeRides: Array,
+    cart: Array
+},{
+    versionKey:null
+})
+const UserModel=mongoose.model('rent_a_ride_user',userSchema)
+module.exports={
+ UserModel
+}
---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const app=express()
+const connection=require('./db')
+const { userRouter } = require('./Routes/users.routes')
+const cors=require('cors')
+app.use(cors())
+require("dotenv").config()
+app.use(express.json())
+app.use('/users',userRouter)
+app.get('/',(req,res)=>{
+    res.send({"msg":"This is the home page for testinf the server"})
+})
+app.listen(4500,async()=>{
+    await connection
+    console.log("App connected to atlas")
+    console.log("App running on port 4500")
+})
